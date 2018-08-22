1da78e8f9917bb6d641c724206bff8735113bd04
amywieliczka <amywieliczka@gmail.com>
2018-08-21 18:12:21 -0700

    gulp build getting there

--- gulpfile.js
+++ gulpfile.js
@@ -12,6 +12,7 @@ const fileinclude = require('gulp-file-include');
 const jshint = require('gulp-jshint');
 const uglify = require('gulp-uglify');
 const stylish = require('jshint-stylish');
+const imagemin = require('gulp-imagemin');
 const gulp = require('gulp');
 
 gulp.task('sass-build', function() {
@@ -57,7 +58,7 @@ gulp.task('js-serve', function() {
 });
 
 gulp.task('html-serve', function(done) {
-  return gulp.src('./app/*.html')
+  return gulp.src('app/(,**/}*.html')
   .pipe(wiredep())
   .pipe(fileinclude({
     basepath: 'app/'
@@ -65,14 +66,47 @@ gulp.task('html-serve', function(done) {
   .pipe(gulp.dest('.tmp'));
 });
 
-gulp.task('html-build', function(done) {
-  return gulp.src('./app/*.html')
+gulp.task('html-build', function() {
+  return gulp.src('app/*.html')
   .pipe(wiredep())
   .pipe(useref({ searchPath: 'dist' }))
   .pipe(fileinclude({
     basepath: 'app/'
   }))
+  // replace html blocks, noAssets set to true - already made bundles
+  .pipe(useref({ noAssets: true, searchPath: 'dist' }))
   .pipe(gulp.dest('dist'));
+});
+
+gulp.task('copy-files', function() {
+  return gulp.src([
+    'app/*.{ico,png,txt}',
+    'app/images/{,**/}*.webp',
+    'node_modules/apache-server-configs/dist/.htaccess'])
+  .pipe(gulp.dest('dist'));
+});
+
+gulp.task('copy-bower-files', function() {
+  return gulp.src([
+    'bower_components/fontawesome',
+    'bower_components/openseadragon',
+    'bower_components/bootstrap-sass-official',
+  ])
+  .pipe(gulp.dest('dist/bower_components/'))
+})
+
+gulp.task('font-build', function() {
+  return gulp.src([
+    'app/styles/fonts/{,**/}*.*',
+    'app/fonts/{,**/}*.*'
+  ])
+  .pipe(gulp.dest('dist/fonts'));
+});
+
+gulp.task('image-build', function() {
+  return gulp.src('app/images/{,**/}*.{gif,jpeg,jpg,png,svg}')
+  .pipe(imagemin())
+  .pipe(gulp.dest('dist/images'))
 });
 
 gulp.task('clean', function() { return del(['./.tmp', './dist']); });
@@ -108,7 +142,7 @@ gulp.task('runserver', function() {
 gulp.task('serve', gulp.series('clean',
 	gulp.parallel('html-serve', 'sass-serve', 'js-serve'), 'runserver'));
 
-gulp.task('build', gulp.series('clean', gulp.parallel('sass-build', 'js-build'), 'html-build'));
+gulp.task('build', gulp.series('clean', gulp.parallel('sass-build', 'js-build', 'image-build', 'copy-files', 'font-build', 'copy-bower-files'), 'html-build'));
 
 
 

a675ba32764f5d35ea2e2cd73c517429105a935a
amywieliczka <amywieliczka@gmail.com>
2018-09-05 17:21:51 -0700

    removed lots of pngs that were ending up in the repo due to backstop.js

--- backstop_data/html_report/config.js
+++ backstop_data/html_report/config.js
@@ -3,11 +3,83 @@ report({
   "tests": [
     {
       "pair": {
-        "reference": "../bitmaps_reference/calisphere_Back_after_Download_0_document_0_phone.png",
-        "test": "../bitmaps_test/20180905-163812/calisphere_Back_after_Download_0_document_0_phone.png",
+        "reference": "../bitmaps_reference/calisphere_BackstopJS_Homepage_0_document_0_phone.png",
+        "test": "../bitmaps_test/20180905-171233/calisphere_BackstopJS_Homepage_0_document_0_phone.png",
         "selector": "document",
-        "fileName": "calisphere_Back_after_Download_0_document_0_phone.png",
-        "label": "Back after Download",
+        "fileName": "calisphere_BackstopJS_Homepage_0_document_0_phone.png",
+        "label": "BackstopJS Homepage",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "url": "https://garris.github.io/BackstopJS/",
+        "referenceUrl": "",
+        "expect": 0,
+        "viewportLabel": "phone",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00"
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/calisphere_BackstopJS_Homepage_0_document_1_tablet.png",
+        "test": "../bitmaps_test/20180905-171233/calisphere_BackstopJS_Homepage_0_document_1_tablet.png",
+        "selector": "document",
+        "fileName": "calisphere_BackstopJS_Homepage_0_document_1_tablet.png",
+        "label": "BackstopJS Homepage",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "url": "https://garris.github.io/BackstopJS/",
+        "referenceUrl": "",
+        "expect": 0,
+        "viewportLabel": "tablet",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00"
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/calisphere_BackstopJS_Homepage_0_document_2_computer.png",
+        "test": "../bitmaps_test/20180905-171233/calisphere_BackstopJS_Homepage_0_document_2_computer.png",
+        "selector": "document",
+        "fileName": "calisphere_BackstopJS_Homepage_0_document_2_computer.png",
+        "label": "BackstopJS Homepage",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "url": "https://garris.github.io/BackstopJS/",
+        "referenceUrl": "",
+        "expect": 0,
+        "viewportLabel": "computer",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00"
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/calisphere_PJAX_Back_After_Download_0_document_0_phone.png",
+        "test": "../bitmaps_test/20180905-171233/calisphere_PJAX_Back_After_Download_0_document_0_phone.png",
+        "selector": "document",
+        "fileName": "calisphere_PJAX_Back_After_Download_0_document_0_phone.png",
+        "label": "PJAX Back After Download",
         "misMatchThreshold": 0.1,
         "url": "https://calisphere.org/UCSF/items/",
         "expect": 0,
@@ -25,11 +97,11 @@ report({
     },
     {
       "pair": {
-        "reference": "../bitmaps_reference/calisphere_Back_after_Download_0_document_1_tablet.png",
-        "test": "../bitmaps_test/20180905-163812/calisphere_Back_after_Download_0_document_1_tablet.png",
+        "reference": "../bitmaps_reference/calisphere_PJAX_Back_After_Download_0_document_1_tablet.png",
+        "test": "../bitmaps_test/20180905-171233/calisphere_PJAX_Back_After_Download_0_document_1_tablet.png",
         "selector": "document",
-        "fileName": "calisphere_Back_after_Download_0_document_1_tablet.png",
-        "label": "Back after Download",
+        "fileName": "calisphere_PJAX_Back_After_Download_0_document_1_tablet.png",
+        "label": "PJAX Back After Download",
         "misMatchThreshold": 0.1,
         "url": "https://calisphere.org/UCSF/items/",
         "expect": 0,
@@ -47,11 +119,11 @@ report({
     },
     {
       "pair": {
-        "reference": "../bitmaps_reference/calisphere_Back_after_Download_0_document_2_computer.png",
-        "test": "../bitmaps_test/20180905-163812/calisphere_Back_after_Download_0_document_2_computer.png",
+        "reference": "../bitmaps_reference/calisphere_PJAX_Back_After_Download_0_document_2_computer.png",
+        "test": "../bitmaps_test/20180905-171233/calisphere_PJAX_Back_After_Download_0_document_2_computer.png",
         "selector": "document",
-        "fileName": "calisphere_Back_after_Download_0_document_2_computer.png",
-        "label": "Back after Download",
+        "fileName": "calisphere_PJAX_Back_After_Download_0_document_2_computer.png",
+        "label": "PJAX Back After Download",
         "misMatchThreshold": 0.1,
         "url": "https://calisphere.org/UCSF/items/",
         "expect": 0,

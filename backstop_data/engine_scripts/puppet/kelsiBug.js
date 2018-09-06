fdeccd140fe3471d1a0d414b7b90761c6881808e
amywieliczka <amywieliczka@gmail.com>
2018-09-05 17:07:00 -0700

    Kelsi Bug now properly tested for

--- backstop_data/engine_scripts/puppet/kelsiBug.js
+++ backstop_data/engine_scripts/puppet/kelsiBug.js
@@ -1,22 +1,66 @@
+// references bug https://www.pivotaltracker.com/story/show/151812193
+// where after a user downloads an image and navigates back to search results
+// the search results page has lost context despite initally looking the same
+// (can't paginate forward, X out search within pill)
+
 module.exports = async (page, scenario, vp) => {
-  console.log('SCENARIO > ' + scenario.label);
-  await require('./clickAndHoverHelper')(page, scenario);
+	console.log('SCENARIO > ' + scenario.label);
+	await require('./clickAndHoverHelper')(page, scenario);
 
-  // add more ready handlers here...
-  const searchElementHandle = await page.$('#search-collection__field');
-  await searchElementHandle.type('doctor');
+	// add more ready handlers here...
+	let searchWithinElementHandle = await page.$('#search-collection__field');
+	await searchWithinElementHandle.type('doctor');
 
-  await Promise.all([
-  	page.waitForResponse(response => response.status() === 200),
-  	searchElementHandle.press('Enter'),
-  ]).then(function() {
-  	console.log('successfully searched for "doctor"');
-  }).catch(function() {
-  	console.log('unsuccessfully search for "doctor"');
-  });
+	// search UCSF items for 'doctor' & get number of results found
+	let searchForDoctor = await Promise.all([
+		page.waitForResponse(response => response.status() === 200),
+		searchWithinElementHandle.press('Enter'),
+  	]);
+  	let resultsFound = await page.$eval('h3.text__collections-heading', el => el.innerText);
 
-  await Promise.all([
-  	page.waitForNavigation(),
-  	page.click('.js-item-link'),
-  ]);
+  	// click into the first item
+  	let navigateToFirstItem = await Promise.all([
+		page.waitForNavigation(),
+		page.waitFor(1000),
+		page.click('.js-item-link'),
+	]);
+
+  	if (vp.label !== 'phone') {
+		// open the download modal
+	  	let downloadModal = await Promise.all([
+			page.waitFor('#downloadModal', {visible: true}),
+			page.waitFor(1000),
+	  		page.click('.obj-buttons__download-image')
+	  	]);
+	  	// click the download button
+	  	let downloadImage = await Promise.all([
+	  		page.waitForNavigation(),
+	  		page.click('#downloadModal .btn-primary')
+	  	]);
+	  	// press the back button to get back to item page
+	  	let backBtn = await page.goBack();
+	}
+	// press the back button to get back to search results
+	let backBtn = await page.goBack();
+
+  	// click 'next' on search results to paginate
+  	nextBtn = (vp.label === 'phone') ? '.button__next' : '.js-next';
+  	let nextResults = await Promise.all([
+  		page.waitForResponse(response => response.status() === 200),
+  		page.waitFor(1000),
+  		page.click(nextBtn),
+  	]);
+
+  	// make sure results found is still the same
+	let newResultsFound = await page.$eval('.text__collections-heading', el => el.innerText);
+	if (newResultsFound !== resultsFound) {
+		throw new Error("results found after navigating 'back' don't match original number of results found");
+	}
+
+	// remove doctor pill
+	let removeDoctor = await Promise.all([
+		page.waitForResponse(response => response.status() === 200),
+		page.waitFor(1000),
+		page.click('.js-refine-filter-pill')
+	]);
 };
--- backstop_data/html_report/config.js
+++ backstop_data/html_report/config.js
@@ -3,80 +3,8 @@ report({
   "tests": [
     {
       "pair": {
-        "reference": "../bitmaps_reference/calisphere_BackstopJS_Homepage_0_document_0_phone.png",
-        "test": "../bitmaps_test/20180831-171920/calisphere_BackstopJS_Homepage_0_document_0_phone.png",
-        "selector": "document",
-        "fileName": "calisphere_BackstopJS_Homepage_0_document_0_phone.png",
-        "label": "BackstopJS Homepage",
-        "requireSameDimensions": true,
-        "misMatchThreshold": 0.1,
-        "url": "https://garris.github.io/BackstopJS/",
-        "referenceUrl": "",
-        "expect": 0,
-        "viewportLabel": "phone",
-        "diff": {
-          "isSameDimensions": true,
-          "dimensionDifference": {
-            "width": 0,
-            "height": 0
-          },
-          "misMatchPercentage": "0.00"
-        }
-      },
-      "status": "pass"
-    },
-    {
-      "pair": {
-        "reference": "../bitmaps_reference/calisphere_BackstopJS_Homepage_0_document_1_tablet.png",
-        "test": "../bitmaps_test/20180831-171920/calisphere_BackstopJS_Homepage_0_document_1_tablet.png",
-        "selector": "document",
-        "fileName": "calisphere_BackstopJS_Homepage_0_document_1_tablet.png",
-        "label": "BackstopJS Homepage",
-        "requireSameDimensions": true,
-        "misMatchThreshold": 0.1,
-        "url": "https://garris.github.io/BackstopJS/",
-        "referenceUrl": "",
-        "expect": 0,
-        "viewportLabel": "tablet",
-        "diff": {
-          "isSameDimensions": true,
-          "dimensionDifference": {
-            "width": 0,
-            "height": 0
-          },
-          "misMatchPercentage": "0.00"
-        }
-      },
-      "status": "pass"
-    },
-    {
-      "pair": {
-        "reference": "../bitmaps_reference/calisphere_BackstopJS_Homepage_0_document_2_computer.png",
-        "test": "../bitmaps_test/20180831-171920/calisphere_BackstopJS_Homepage_0_document_2_computer.png",
-        "selector": "document",
-        "fileName": "calisphere_BackstopJS_Homepage_0_document_2_computer.png",
-        "label": "BackstopJS Homepage",
-        "requireSameDimensions": true,
-        "misMatchThreshold": 0.1,
-        "url": "https://garris.github.io/BackstopJS/",
-        "referenceUrl": "",
-        "expect": 0,
-        "viewportLabel": "computer",
-        "diff": {
-          "isSameDimensions": true,
-          "dimensionDifference": {
-            "width": 0,
-            "height": 0
-          },
-          "misMatchPercentage": "0.00"
-        }
-      },
-      "status": "pass"
-    },
-    {
-      "pair": {
         "reference": "../bitmaps_reference/calisphere_Back_after_Download_0_document_0_phone.png",
-        "test": "../bitmaps_test/20180831-171920/calisphere_Back_after_Download_0_document_0_phone.png",
+        "test": "../bitmaps_test/20180905-163812/calisphere_Back_after_Download_0_document_0_phone.png",
         "selector": "document",
         "fileName": "calisphere_Back_after_Download_0_document_0_phone.png",
         "label": "Back after Download",
@@ -90,17 +18,15 @@ report({
             "width": 0,
             "height": 0
           },
-          "misMatchPercentage": "0.60",
-          "analysisTime": 60
-        },
-        "diffImage": "../bitmaps_test/20180831-171920/failed_diff_calisphere_Back_after_Download_0_document_0_phone.png"
+          "misMatchPercentage": "0.00"
+        }
       },
-      "status": "fail"
+      "status": "pass"
     },
     {
       "pair": {
         "reference": "../bitmaps_reference/calisphere_Back_after_Download_0_document_1_tablet.png",
-        "test": "../bitmaps_test/20180831-171920/calisphere_Back_after_Download_0_document_1_tablet.png",
+        "test": "../bitmaps_test/20180905-163812/calisphere_Back_after_Download_0_document_1_tablet.png",
         "selector": "document",
         "fileName": "calisphere_Back_after_Download_0_document_1_tablet.png",
         "label": "Back after Download",
@@ -109,22 +35,20 @@ report({
         "expect": 0,
         "viewportLabel": "tablet",
         "diff": {
-          "isSameDimensions": false,
+          "isSameDimensions": true,
           "dimensionDifference": {
             "width": 0,
-            "height": -110
+            "height": 0
           },
-          "misMatchPercentage": "1.03",
-          "analysisTime": 83
-        },
-        "diffImage": "../bitmaps_test/20180831-171920/failed_diff_calisphere_Back_after_Download_0_document_1_tablet.png"
+          "misMatchPercentage": "0.00"
+        }
       },
-      "status": "fail"
+      "status": "pass"
     },
     {
       "pair": {
         "reference": "../bitmaps_reference/calisphere_Back_after_Download_0_document_2_computer.png",
-        "test": "../bitmaps_test/20180831-171920/calisphere_Back_after_Download_0_document_2_computer.png",
+        "test": "../bitmaps_test/20180905-163812/calisphere_Back_after_Download_0_document_2_computer.png",
         "selector": "document",
         "fileName": "calisphere_Back_after_Download_0_document_2_computer.png",
         "label": "Back after Download",
@@ -138,12 +62,10 @@ report({
             "width": 0,
             "height": 0
           },
-          "misMatchPercentage": "0.80",
-          "analysisTime": 57
-        },
-        "diffImage": "../bitmaps_test/20180831-171920/failed_diff_calisphere_Back_after_Download_0_document_2_computer.png"
+          "misMatchPercentage": "0.00"
+        }
       },
-      "status": "fail"
+      "status": "pass"
     }
   ],
   "id": "calisphere"

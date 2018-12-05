90fb54efedaa1ca2afda006dc4fadf800c5ff9bc
amywieliczka <amywieliczka@gmail.com>
2018-12-05 12:35:35 -0800

    Added aria-expanded attribute to global header and modified JS to change it

--- app/scripts/calisphere-home.js
+++ app/scripts/calisphere-home.js
@@ -8,8 +8,13 @@ $(document).ready(function() {
     });
 
     $(document).on('click', '.js-global-header__bars-icon', function() {
-      $('.js-global-header__search').toggleClass('global-header__search global-header__search--selected');
+      $('.js-global-header__search').toggleClass('.global-header__search global-header__search--selected');
       $('.js-global-header__mobile-links').toggleClass('.global-header__mobile-links global-header__mobile-links--selected');
+      if ($('.js-global-header__mobile-links').is(':visible')) {
+        $('.js-global-header__mobile-links').attr('aria-expanded', true);
+      } else {
+        $('.js-global-header__mobile-links').attr('aria-expanded', false);
+      }
     });
 
     // Toggle only search box:

d8bf4f235b6a91ecfb15af576d763cbd33e026a7
amywieliczka <amywieliczka@gmail.com>
2019-02-20 14:37:49 -0800

    fix non-ascii quotes in copy/pasted comments

--- exhibits/templatetags/markdown_filter.py
+++ exhibits/templatetags/markdown_filter.py
@@ -4,7 +4,7 @@ from markdown.extensions import Extension
 
 # https://github.com/Python-Markdown/markdown/commit/7db56daedf8a6006222f55eeeab748e7789fba89 
 # per notes in the release docs for version 2.5, used an EscapeHTML 
-# extension to replace safe_mode=”escape” (deprecated)
+# extension to replace safe_mode="escape" (deprecated)
 
 class EscapeHtml(Extension):
 	def extendMarkdown(self, md, md_globals):

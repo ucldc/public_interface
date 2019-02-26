98764480c2bbd792219f749d2af366ad38a5d4a6
amywieliczka <amywieliczka@gmail.com>
2019-02-26 14:58:58 -0800

    updates recommended by 2to3 package

--- calisphere/sitemap_generator.py
+++ calisphere/sitemap_generator.py
@@ -1,4 +1,4 @@
-from __future__ import unicode_literals, print_function
+
 from builtins import next
 from builtins import range
 import os
@@ -62,7 +62,7 @@ class CalisphereSitemapGenerator(SitemapGenerator):
     def write_data_fast(self, section, site):
         ''' process data using generator and streaming xml '''
         # FIXME need to generalize this code if we ever want to use it for anything other than Calisphere items
-        items = site().items()  # generator yielding all items
+        items = list(site().items())  # generator yielding all items
 
         parts = []
 

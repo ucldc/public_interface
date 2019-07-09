68269e35a92db20dbf0a3c2367d88a24c48e9f4d
amywieliczka <amywieliczka@gmail.com>
2019-07-09 15:55:55 -0700

    retrieve custom facet sort_by field from the registry api

--- calisphere/views.py
+++ calisphere/views.py
@@ -507,7 +507,7 @@ def itemViewCarousel(request):
                     custom_facet['facet_field'],
                     custom_facet['label'],
                     custom_facet['facet_field'],
-                    'value'
+                    custom_facet.get('sort_by', 'count')
                 )
             )
     elif referral == 'campus':
@@ -835,7 +835,7 @@ def collectionView(request, collection_id):
                     custom_facet['facet_field'],
                     custom_facet['label'],
                     custom_facet['facet_field'],
-                    'value'
+                    custom_facet.get('sort_by', 'count')
                 )
             )
     extra_filter = 'collection_url: "' + collection_url + '"'

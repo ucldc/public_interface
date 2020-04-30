c2b13efbda843e3d19642a89342a35928b246e19
amywieliczka <amywieliczka@gmail.com>
2020-04-29 20:22:22 -0700

    integrated the new header on the homepage

--- app/scripts/calisphere-home.js
+++ app/scripts/calisphere-home.js
@@ -7,20 +7,71 @@ $(document).ready(function() {
       el.reset();
     });
 
-    $(document).on('click', '.js-global-header__bars-icon', function() {
-      $('.js-global-header__search').toggleClass('.global-header__search global-header__search--selected');
-      $('.js-global-header__mobile-links').toggleClass('.global-header__mobile-links global-header__mobile-links--selected');
-      if ($('.js-global-header__mobile-links').is(':visible')) {
-        $('.js-global-header__mobile-links').attr('aria-expanded', true);
-      } else {
-        $('.js-global-header__mobile-links').attr('aria-expanded', false);
-      }
-    });
+    var navOpenState;
+    var searchOpenState;
 
-    // Toggle only search box:
-    //  $('.js-global-header__search-icon').click(function(){
-    $(document).on('click', '.js-global-header__search-icon', function() {
-      $('.js-global-header__search').toggleClass('global-header__search global-header__search--selected');
-    });
+    function setAttrs (headerElState, headerEl, headerElButton) {
+      if (headerElState === false) {
+        headerEl.classList.add('is-closed');
+        headerEl.classList.remove('is-open');
+        headerElButton.setAttribute('aria-expanded', false);
+      } else {
+        headerEl.classList.add('is-open');
+        headerEl.classList.remove('is-closed');
+        headerElButton.setAttribute('aria-expanded', true);
+      }
+    }
+
+    function watchHeaderWidth (headerWidth) {
+      var mobileSearch = document.querySelector('.header__search');
+      var mobileSearchButton = document.querySelector('.header__mobile-search-button');
+      var mobileNav = document.querySelector('.header__nav');
+      var mobileNavButton = document.querySelector('.header__mobile-nav-button');
+
+      if (headerWidth.matches) {
+        navOpenState = true;
+        searchOpenState = true;
+      } else {
+        navOpenState = false;
+        searchOpenState = false;
+      }
+
+      if (mobileSearch) {
+        setAttrs(searchOpenState, mobileSearch, mobileSearchButton);
+      }
+      setAttrs(navOpenState, mobileNav, mobileNavButton);
+    }
+
+    if (document.querySelector('.header')) {
+      var mobileNavButton = document.querySelector('.header__mobile-nav-button');
+      mobileNavButton.addEventListener('click', function (e) {
+        var mobileNav = document.querySelector('.header__nav');
+
+        if (navOpenState === true) {
+          navOpenState = false;
+        } else {
+          navOpenState = true;
+        }
+        setAttrs(navOpenState, mobileNav, e.currentTarget);
+      });
+
+      var mobileSearchButton = document.querySelector('.header__mobile-search-button');
+      if (mobileSearchButton) {
+        mobileSearchButton.addEventListener('click', function (e) {
+          var mobileSearch = document.querySelector('.header__search');
+          if (searchOpenState === true) {
+            searchOpenState = false;
+          } else {
+            searchOpenState = true;
+          }
+          setAttrs(searchOpenState, mobileSearch, e.currentTarget);
+        });
+      }
+
+      var headerWidth = window.matchMedia('(min-width: 650px)');
+      // Watch screen width and update nav/search open states:
+      watchHeaderWidth(headerWidth);
+      headerWidth.addListener(watchHeaderWidth);
+    }
   }
 });

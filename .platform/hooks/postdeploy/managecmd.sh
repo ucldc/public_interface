c6d7dec145967a351d19d286d4faecd36383fb00
Amy Wieliczka <amywieliczka@gmail.com>
2022-06-13 17:22:13 +0000

    collectstatics needs to be in pre and post deploy

---
+++ .platform/hooks/postdeploy/managecmd.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+source /var/app/venv/*/bin/activate
+echo "starting collect statics";
+python manage.py collectstatic --noinput
+echo "starting site maps";
+python manage.py calisphere_refresh_sitemaps --settings public_interface.settings
+

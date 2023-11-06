8e92bda3c196d6f8d0a03e2d112a034d8c8d5e9a
amy wieliczka <amywieliczka@gmail.com>
2023-11-06 11:40:58 -0800

    Update managecmd.sh

--- .platform/hooks/postdeploy/managecmd.sh
+++ .platform/hooks/postdeploy/managecmd.sh
@@ -5,4 +5,5 @@ echo "starting collect statics";
 python manage.py collectstatic --noinput
 echo "starting site maps";
 python manage.py calisphere_refresh_sitemaps --settings public_interface.settings
+ls -la static_root
 

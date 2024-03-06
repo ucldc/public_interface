38c8101b34b7eec5d7ff80523aede8e69682ccc0
Barbara Hui <barbara.hui@ucop.edu>
2024-03-06 11:18:41 -0800

    Update item_manager.py to send queries to es_cache_retry only

--- calisphere/item_manager.py
+++ calisphere/item_manager.py
@@ -1,38 +1,14 @@
-from .cache_retry import search_solr, SOLR_get, SOLR_mlt
-try:
-    from .es_cache_retry import search_es, es_get, es_mlt
-    ES = True
-except ImportError:
-    ES = False
-
+from .es_cache_retry import search_es, es_get, es_mlt
 
 class ItemManager(object):
-    def __init__(self, index='solr'):
-        if ES is True:
-            self.index = index
-        else:
-            self.index = 'solr'
+    def __init__(self, index='es'):
+        self.index = index
 
     def search(self, query):
-        if self.index == "solr":
-            results = search_solr(query)
-        elif self.index == "es":
-            results = search_es(query)
-        return results
+        return search_es(query)
 
     def get(self, item_id):
-        if self.index == 'es':
-            item_search = es_get(item_id)
-        else:
-            item_id_search_term = 'id:"{0}"'.format(item_id)
-            item_search = SOLR_get(q=item_id_search_term)
-        return item_search
+        return es_get(item_id)
 
     def more_like_this(self, item_id):
-        if self.index == 'es':
-            results = es_mlt(item_id)
-        else:
-            results = SOLR_mlt(item_id)
-        return results
-
-
+        return es_mlt(item_id)

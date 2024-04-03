f587932026b3b5b16b1af1b0d3ebff760265bf18
amy wieliczka <amywieliczka@gmail.com>
2024-04-02 17:28:25 -0700

    Don't just set local index var, set request.session['index']

--- calisphere/decorators.py
+++ calisphere/decorators.py
@@ -7,12 +7,20 @@ from django.views.decorators.cache import cache_page
 def cache_by_session_state(func):
     @wraps(func, assigned=available_attrs(func))
     def wrapper(request, *args, **kwargs):
-        index = request.session.get('index', settings.DEFAULT_INDEX)
+        index = request.session.get('index')
+
         if (
+            not index or
             (index == 'solr' and not settings.SOLR_URL) or
             (index == 'es' and not settings.ES_HOST)
         ):
+            # no index happens when we have a first-time visitor;
+            # index variables without corresponding settings variables
+            # can happen in development when there's a persistent
+            # browser session across a change in settings configuration
             index = settings.DEFAULT_INDEX
+            request.session['index'] = index
+
         cached = cache_page(60 * 1, key_prefix=index)(func)
         return cached(request, *args, **kwargs)
     return wrapper

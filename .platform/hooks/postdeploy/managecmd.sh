ec76e96e55e81bda0e14e7f953e4c10c2528906f
amy wieliczka <amywieliczka@gmail.com>
2024-04-04 17:19:37 -0700

    Download static sitemaps rather than generate them (#306)

--- .platform/hooks/postdeploy/managecmd.sh
+++ .platform/hooks/postdeploy/managecmd.sh
@@ -3,7 +3,7 @@
 source /var/app/venv/*/bin/activate
 echo "starting collect statics";
 python manage.py collectstatic --noinput
-echo "finished collect statis; starting site maps";
-python manage.py calisphere_refresh_sitemaps --settings public_interface.settings
-echo "finished site maps";
-
+echo "finished collect statics";
+echo "start downloading sitemaps";
+aws s3 cp s3://static-ucldc-cdlib-org/sitemaps/2024-04-03-calisphere-prd/ /var/app/current/sitemaps/ --recursive || true
+echo "finished downloading sitemaps";

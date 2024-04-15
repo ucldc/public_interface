361db5a287791b19dfa28f688c354b99c3c5959c
amy wieliczka <amywieliczka@gmail.com>
2024-04-15 13:52:07 -0700

    another attempt to redirect long output out of eb-hooks.log

--- .platform/hooks/postdeploy/managecmd.sh
+++ .platform/hooks/postdeploy/managecmd.sh
@@ -9,5 +9,5 @@ echo "finished collectstatic";
 echo "start downloading sitemaps";
 sitemaps=s3://static-ucldc-cdlib-org/sitemaps/2024-04-03-calisphere-prd/
 echo "post-deploy: aws s3 cp $sitemaps /var/app/current/sitemaps/ --recursive" >> /var/log/s3_download.log
-aws s3 cp s3://static-ucldc-cdlib-org/sitemaps/2024-04-03-calisphere-prd/ /var/app/current/sitemaps/ --recursive || true >> /var/log/s3_download.log 2>&1
+aws s3 cp s3://static-ucldc-cdlib-org/sitemaps/2024-04-03-calisphere-prd/ /var/app/current/sitemaps/ --recursive >> /var/log/s3_download.log 2>&1 || true
 echo "finished downloading sitemaps";

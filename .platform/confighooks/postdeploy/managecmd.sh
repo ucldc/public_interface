b1f39b02f7ad229877fa1cf0db0d8f23ce914c44
Barbara Hui <barbara.hui@ucop.edu>
2024-04-22 14:29:17 -0700

    Copy .platform/hooks to new dir .platform/confighooks

---
+++ .platform/confighooks/postdeploy/managecmd.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+source /var/app/venv/*/bin/activate
+echo "starting collectstatic";
+echo "post-deploy: python manage.py collectstatic --noinput" >> /var/log/collectstatic.log
+python manage.py collectstatic --noinput >> /var/log/collectstatic.log 2>&1
+echo "finished collectstatic";
+
+echo "start downloading sitemaps";
+sitemaps=s3://static-ucldc-cdlib-org/sitemaps/2024-04-03-calisphere-prd/
+echo "post-deploy: aws s3 cp $sitemaps /var/app/current/sitemaps/ --recursive" >> /var/log/s3_download.log
+aws s3 cp s3://static-ucldc-cdlib-org/sitemaps/2024-04-03-calisphere-prd/ /var/app/current/sitemaps/ --recursive >> /var/log/s3_download.log 2>&1 || true
+echo "finished downloading sitemaps";

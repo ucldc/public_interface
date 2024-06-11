f21d6a058e6e7a5db22c3105b41aad4ae584ef95
amy wieliczka <amywieliczka@gmail.com>
2024-06-10 22:18:13 -0700

    Update predeploy and postdeploy config hooks with new sitemap location

--- .platform/hooks/postdeploy/managecmd.sh
+++ .platform/hooks/postdeploy/managecmd.sh
@@ -7,7 +7,7 @@ python manage.py collectstatic --noinput >> /var/log/collectstatic.log 2>&1
 echo "finished collectstatic";
 
 echo "start downloading sitemaps";
-sitemaps=s3://static-ucldc-cdlib-org/sitemaps/2024-04-03-calisphere-prd/
+sitemaps=s3://calisphere-static/sitemaps/
 echo "post-deploy: aws s3 cp $sitemaps /var/app/current/sitemaps/ --recursive" >> /var/log/s3_download.log
-aws s3 cp s3://static-ucldc-cdlib-org/sitemaps/2024-04-03-calisphere-prd/ /var/app/current/sitemaps/ --recursive >> /var/log/s3_download.log 2>&1 || true
+aws s3 cp s3://calisphere-static/sitemaps/ /var/app/current/sitemaps/ --recursive >> /var/log/s3_download.log 2>&1 || true
 echo "finished downloading sitemaps";

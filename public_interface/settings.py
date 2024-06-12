57e9d6f55d666a26f6366e38b693bbcb1ab21cd1
Barbara Hui <barbara.hui@ucop.edu>
2024-06-12 10:15:58 -0700

    Set sentry profiling rate to 50%

--- public_interface/settings.py
+++ public_interface/settings.py
@@ -27,7 +27,7 @@ if os.environ.get('UCLDC_FRONT') == 'https://calisphere.org/':
         # Set profiles_sample_rate to 1.0 to profile 100%
         # of sampled transactions.
         # We recommend adjusting this value in production.
-        profiles_sample_rate=1.0,
+        profiles_sample_rate=0.5,
     )
 
 def getenv(variable, default):

a7497ce4a1680606f93392ebc22bb2c262965fee
Barbara Hui <barbara.hui@ucop.edu>
2024-06-20 16:17:57 -0700

    Move import statements to top of file

--- public_interface/settings.py
+++ public_interface/settings.py
@@ -15,16 +15,17 @@ from django.utils.crypto import get_random_string  # http://stackoverflow.com/a/
 import os
 import sys
 
+import boto3
+import sentry_sdk
+
 # don't initialize Sentry if we're in a local dev environment
 if bool(os.environ.get('EB_ENVIRONMENT_NAME')):
     eb_env_name = os.environ.get('EB_ENVIRONMENT_NAME')
-    import boto3
     eb_client = boto3.client('elasticbeanstalk')
     eb_env_description = eb_client.describe_environments(
         ApplicationName='eb-calisphere', EnvironmentNames=[eb_env_name])
     eb_app_version = eb_env_description['Environments'][0]['VersionLabel']
 
-    import sentry_sdk
     # https://docs.sentry.io/platforms/python/integrations/django/
     sentry_sdk.init(
         dsn="https://31e2ccf069b4049859fb97c8784ba33c@o1065376.ingest.us.sentry.io/4507346951274496",
